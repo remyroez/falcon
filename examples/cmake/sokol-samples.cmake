a69c6b302d346dda0dc7075b236579e5d08d7b25
れみろう <remyroez+github@gmail.com>
2020-08-13 00:40:29 +0900

    :wrench: Setup CMake modules for use shader.

---
+++ examples/cmake/sokol-samples.cmake
@@ -0,0 +1,3 @@
+# vars
+set(SOKOL_SAMPLES_PATH ${CMAKE_SOURCE_DIR}/thirdparty/sokol-samples)
+set(SOKOL_SAMPLES_SAPP_PATH ${SOKOL_SAMPLES_PATH}/sapp)
---
+++ examples/cmake/sokol-shdc.cmake
@@ -0,0 +1,3 @@
+# vars
+set(SOKOL_TOOLS_BIN_PATH ${CMAKE_SOURCE_DIR}/thirdparty/sokol-tools-bin)
+set(SOKOL_SHDC_PATH ${SOKOL_TOOLS_BIN_PATH}/bin/linux/sokol-shdc)
